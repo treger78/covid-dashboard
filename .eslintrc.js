ed499339d6897154a15bc629a0f7bd43a066358b
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2020-12-19 10:00:47 +0500

    docs: add webpack and eslint config files

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {
+    semi: 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.json'],
+      },
+    },
+  },
+};
