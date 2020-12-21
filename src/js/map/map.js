26bd2efe452709474cbfbb1cb1fedcd14256cd95
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2020-12-21 09:25:00 +0500

    feat: some changings in map block structure and create map obj on page

--- src/js/map/map.js
+++ src/js/map/map.js
@@ -1,4 +1,9 @@
-// You can change everything by your discretion
+import L from 'leaflet';
 
+const map = new L.Map('map', { center: [40, 20], zoom: 2 });
+const layer = new L.TileLayer(
+  'https://api.mapbox.com/styles/v1/abdulloh76/ckiuab9eh2phd19qk49l16mvl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWJkdWxsb2g3NiIsImEiOiJja2l1YTV6Z3EwOWdhMnpuenE3N3gxOHhjIn0.txVoKFibwsl1bMD2ylfxOA',
+);
+map.addLayer(layer);
 export const mapInit = (data) => data;
 export const mapConvert = (data) => data;
